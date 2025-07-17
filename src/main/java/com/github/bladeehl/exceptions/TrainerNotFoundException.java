dab3d4d0196283bf8d8201345566771a4c00be36
Simonov Dmitrey <ddluker454@gmail.com>
2025-07-17 12:08:42 +0300

    Добавил кастомные исключения

---
+++ src/main/java/com/github/bladeehl/exceptions/TrainerNotFoundException.java
@@ -0,0 +1,7 @@
+package com.github.bladeehl.exceptions;
+
+public class TrainerNotFoundException extends RuntimeException {
+    public TrainerNotFoundException(final String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/github/bladeehl/exceptions/UnsupportedPokemonTypeException.java
@@ -0,0 +1,7 @@
+package com.github.bladeehl.exceptions;
+
+public class UnsupportedPokemonTypeException extends RuntimeException {
+    public UnsupportedPokemonTypeException(String message) {
+        super(message);
+    }
+}
